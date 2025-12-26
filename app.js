f014971ef2741703a3e86bd2a4cd9dd102d2f79b
Aiman <aiimanfaiz02@gmail.com>
2025-12-26 09:49:24 +0800

    s

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const firebaseConfig = {
     measurementId: "G-JP1Y2S1LN5"
 };
 
-// Initialize Firebase
+// Initialize Firebasesda
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
